237fe7893d31704b202b52686d19b7d1cd74d0b1
ICEPOR\asharma2 <asharma2@ivycomptech.com>
2019-04-29 17:26:48 +0530

    Updated the code to Blazor Preview

--- E2ETestApp/E2ETestApp/Startup.cs
+++ E2ETestApp/E2ETestApp/Startup.cs
@@ -1,4 +1,4 @@
-using Microsoft.AspNetCore.Blazor.Builder;
+using Microsoft.AspNetCore.Components.Builder;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace E2ETestApp
@@ -9,7 +9,7 @@ namespace E2ETestApp
         {
         }
 
-        public void Configure(IBlazorApplicationBuilder app)
+        public void Configure(IComponentsApplicationBuilder app)
         {
             app.AddComponent<App>("app");
         }
